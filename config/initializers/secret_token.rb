6fb1278253b83331ffcc02993a06b0912ce90f42
Ben <falk.ben@gmail.com>
2013-07-10 16:05:44 -0400

    Rails 3 upgrade (3.0.20) Routes need to be fixed - but index actually loads!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimalRecordKeeping::Application.config.secret_token = 'e7e875850e028d340503ec8cc565f0fb02d1a60f2a15b9baa7c81729014276565d6bc6f4ce44fc58a3853281a84c9292d06270656dfc633bac12a587dc065c13'
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,83 @@
-ActionController::Routing::Routes.draw do |map|
+AnimalRecordKeeping::Application.routes.draw do
   
-  map.connect '/protocols/update_mult_bats', :controller => 'protocols', :action => 'update_mult_bats'
-  map.resources :protocols
+	#map.connect '/protocols/update_mult_bats', :controller => 'protocols', :action => 'update_mult_bats'
+  #map.resources :protocols
 
   
   #map.connect '/trainings/new_mult_users', :controller => 'trainings', :action => 'new_mult_users'
   #map.connect '/trainings/create_mult_users', :controller => 'trainings', :action => 'create_mult_users'
-  map.resources :trainings, :new => { :new_mult_users => :get }, :member => { :create_mult_users => :post }
-  # The priority is based upon order of creation: first created -> highest priority.
+  #map.resources :trainings, :new => { :new_mult_users => :get }, :member => { :create_mult_users => :post }
   
-  # Sample of regular route:
-  # map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  # map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # You can have the root of your site routed by hooking up '' 
-  # -- just remember to delete public/index.html.
-  map.connect '', :controller => "main", :action => "index"
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+	root :to => "main#index"
+  #map.connect '', :controller => "main", :action => "index"
 
   # Allow downloading Web Service WSDL as a file with an extension
   # instead of a file named 'wsdl'
-  map.connect ':controller/service.wsdl', :action => 'wsdl'
+  #map.connect ':controller/service.wsdl', :action => 'wsdl'
 
   # Install the default route as the lowest priority.
-  map.connect ':controller/:action/:id'
+  #map.connect ':controller/:action/:id'
   #map.connect ':controller/:action/:id.:format'
+	
+	
+	#########################################################
+	
+	# The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  match ':controller(/:action(/:id(.:format)))'
 end
